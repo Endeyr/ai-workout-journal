4f8568a80770d6750e4eded503a9b593a5ef2850
Endeyr <aaron.thompson42594@gmail.com>
2025-05-27 16:48:44 -0400

    feat: add auth with oauth and google api

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import { auth } from '@/auth';
+import SignIn from '@/components/sign-in';
+import SignOut from '@/components/sign-out';
+import UserAvatar from '@/components/UserAvatar';
 import Link from 'next/link';
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const user = session?.user;
+
+  console.log(user);
+
   return (
     <div>
+      {!user ? <SignIn /> : <SignOut />}
       <h1>Home</h1>
       <Link href='/about'>About</Link>
+      <UserAvatar />
     </div>
   );
 }
