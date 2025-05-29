d33834769e07c991c29ab506c3ab90720be9e8b3
Endeyr <aaron.thompson42594@gmail.com>
2025-05-29 11:01:33 -0400

    feat: add workout table

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -33,4 +33,24 @@ export const exercise_table = createTable(
   }
 );
 
-export type DB_TableType = typeof exercise_table.$inferSelect;
+export type DB_ExerciseTableType = typeof exercise_table.$inferSelect;
+
+export const workout_table = createTable(
+  'table',
+  {
+    id: bigint('id', { mode: 'number', unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    ownerId: text('owner_id').notNull(), // User Id
+    name: text('name').notNull(), // Workout Name
+    type: text('type').notNull(), // Enum? Legs, Arms, Back, ect
+    duration: int().notNull(), // HHMM
+    notes: text('notes'),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+  },
+  (t) => {
+    return [index('owner_id_index').on(t.ownerId)];
+  }
+);
+
+export type DB_WorkoutTableType = typeof exercise_table.$inferSelect;
